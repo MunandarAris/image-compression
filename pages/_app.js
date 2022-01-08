81a0a6701b96b92e52a8e8606a036af4811b5bac
Aris Munandar <arismun0010@gmail.com>
2022-01-08 16:11:07 +0700

    First Upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+// import css from react bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
